2ed16ea4c6e5aa5e3ca8de0aa4c7517ee467ca99
saranraj5 <54973023+saranraj5@users.noreply.github.com>
2019-09-07 13:40:29 +0530

    Update Small.c

--- Small.c
+++ Small.c
@@ -2,23 +2,13 @@
 #include <stdlib.h>
   int main()
 {
-  int n,a[1001];
+  int n,a,min=999999;
   scanf("%d",&n);
   for(int i=0;i<n;i++)
   {
-    scanf("%d",&a[i]);
+    scanf("%d",&a);
+  if(a<min)
+  min=a;
   }
-  for(int i=0;i<n;i++)
-  {
-    for(int j=0;j<n;j++)
-    {
-      if(a[i]<a[j])
-      {
-        int temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
-      }
-    }
-  }
-  printf("%d",a[0]);
+  printf("%d",min);
 }
